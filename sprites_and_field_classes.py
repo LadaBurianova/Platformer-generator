057ba7fedf1019be9c097358c82ca685b3454530
LadaBurianova <bulanova.dasha2004@yandex.ru>
2021-01-11 01:58:44 +0300

    Function load was renamed to load_image; two modes of __init__ were added in class Field; csv functions was added to class Field; description of class Field was added.

--- sprites_and_field_classes.py
+++ sprites_and_field_classes.py
@@ -1,9 +1,10 @@
 import pygame
 from os.path import join, isfile
 from sys import exit
+import csv
 
 
-def load(folder, name):
+def load_image(folder, name):
     full_path = join(folder, name)
     if not isfile(full_path):
         print(f"Файл '{full_path}' не найден")
@@ -12,9 +13,29 @@ def load(folder, name):
 
 
 class Field:
-    def __init__(self, matrix=None):
-        if matrix is None:
-            self.matrix = [[Cell(1) for _ in range(110)] for __ in range(9)] + [[Cell(7)] for _ in range(110)]
+    """There are 2 modes of __init__.
+    1) if mode == 'load' the map will be loaded from lv_filename. It's default mode.
+    2) if mode != 'load' the new map will be created. Use self.save_level function to save the map.  """
+
+    def __init__(self, map_filename, mode='load'):
+        self.f = map_filename
+        if mode == 'load':
+            self.matrix = []
+            self.load_level()
+        else:
+            self.matrix = [[Cell('1') for _ in range(110)] for __ in range(9)] + [[Cell('7')] for _ in range(110)]
+
+    def load_level(self):
+        with open(self.f, 'r', encoding='utf-8') as f_r:
+            reader = csv.reader(f_r, delimiter=';', quotechar='"')
+            for line in reader:
+                self.matrix.append([Cell(i) for i in line])
+
+    def save_level(self):
+        with open(self.f, 'w', encoding='utf-8') as f_w:
+            writer = csv.writer(f_w, delimiter=';', quotechar='"')
+            for line in self.matrix:
+                writer.writerow(line)
 
 
 class Person:
@@ -22,7 +43,7 @@ class Person:
 
     def __init__(self, n):
         super().__init__(self, person_sprites, all_sprites)
-        self.img = load('person', n + Cell.EXP)
+        self.img = load_image('person', n + Cell.EXP)
 
 
 class Cell(pygame.Sprite.sprite):
@@ -30,7 +51,7 @@ class Cell(pygame.Sprite.sprite):
 
     def __init__(self, n):
         super().__init__(self, cells_sprites, all_sprites)
-        self.img = load('cells', n + Cell.EXP)
+        self.img = load_image('cells', n + Cell.EXP)
 
 
 all_sprites = pygame.sprite.Group()
